63817434a62f15388e1075c02aa6b9859f588cf4
ratankumarthakur <144756277+ratankumarthakur@users.noreply.github.com>
2025-07-17 23:00:34 +0530

    Update constants.dart

--- lib/video_call/constants.dart
+++ lib/video_call/constants.dart
@@ -1,4 +1,4 @@
 class appinfo{
-  static const appId=101597341;
-  static const appSign= '2adca6dcd717b62bd4c7b07a1697101454e213d9be9c4f98e98dc8512a240568';
+  static const appId=;
+  static const appSign= '';
 }
